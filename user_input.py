4dd2d1dafd5bf2348f59dd6a25a0a9aa04ff8381
NG-Kalonzo <kalojeff83@gmail.com>
2024-03-08 08:46:34 +0300

    python assigment1

---
+++ user_input.py
@@ -0,0 +1,7 @@
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+location = input("Enter your location: ")
+
+print("You name is {}, you are {} years old and you live in {}.".format(name,age,location))
+
